9fcfe562782bf86e05ea1fe0565a54336d085980
User21 <user21@user21de-imac.local>
2018-06-26 23:52:46 +0800

    Initial Commit

---
+++ 00457021_2Tests/_0457021_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0457021_2Tests.swift
+//  00457021_2Tests
+//
+//  Created by User21 on 2018/6/26.
+//  Copyright © 2018年 User21. All rights reserved.
+//
+
+import XCTest
+@testable import _0457021_2
+
+class _0457021_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
